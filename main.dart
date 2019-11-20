89efaca46e73f8ac11959faa24694dcbd760a627
Nilen Patel <47520992+nilenpatel@users.noreply.github.com>
2019-11-20 21:29:52 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:dynamic_theme/theme_switcher_widgets.dart';
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new DynamicTheme(
+      defaultBrightness: Brightness.light,
+      data: (brightness) => new ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: brightness,
+      ),
+      themedWidgetBuilder: (context, theme) {
+        return new MaterialApp(
+          title: 'Flutter Demo',
+          theme: theme,
+          home: new MyHomePage(title: 'Flutter Demo Home Page'),
+        );
+      }
+    );
+  }
+}
